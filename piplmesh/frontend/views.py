46371076b9cd65771de729490ced66df9d7c5b19
Matevž <matevz.mihalic@gmail.com>
2012-03-04 22:57:37 +0100

    Replace home view with something more "standard".

--- piplmesh/frontend/views.py
+++ piplmesh/frontend/views.py
@@ -1,14 +1,15 @@
-from django.views import generic as generic_views
+from django.http import HttpResponse
+from django.template import loader, RequestContext
+from lazysignup.decorators import allow_lazy_user
 
-class HomeView(generic_views.TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-
-        context.update({
-            'search_engine': 'Google',
-            'search_engine_logo': 'google_logo.png'
-        })
-
-        return context
+@allow_lazy_user
+def homeView(request):
+    
+    c = RequestContext(request,{
+        'search_engine': 'Google',
+        'search_engine_logo': 'google_logo.png'
+    })
+    
+    template = 'home.html'
+    t = loader.get_template(template)
+    return HttpResponse(t.render(c))
