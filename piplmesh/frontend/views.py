4609aec07f38a0ff514bdae01a3f46aaf8620b91
Matevž <matevz.mihalic@gmail.com>
2012-03-07 09:32:05 +0100

    Remove get_context_data which is not needed anymore, because it's empty.

--- piplmesh/frontend/views.py
+++ piplmesh/frontend/views.py
@@ -9,8 +9,3 @@ class LazyUserMixin(object):
 
 class HomeView(LazyUserMixin, generic_views.TemplateView):
     template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-
-        return context
