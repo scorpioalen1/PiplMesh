712e21d416a663d63189eca9c4817c81ec9c2ef9
Matevž <matevz.mihalic@gmail.com>
2012-04-14 01:46:38 +0200

    Removing gravatar template tag which is not needed anymore.

--- piplmesh/settings.py
+++ piplmesh/settings.py
@@ -47,9 +47,6 @@ URL_VALIDATOR_USER_AGENT = 'Django'
 
 SITE_ID = 1
 
-# Use SSL for default gravatar URL
-GRAVATAR_HTTPS_DEFAULT = False
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
