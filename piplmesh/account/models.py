9c9bb4574d9403bc82f7c9aaade559ab68cf3ba0
Matevž <matevz.mihalic@gmail.com>
2012-04-14 01:45:54 +0200

    Reorganizing profile image code.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -1,9 +1,12 @@
-import datetime
+import datetime, hashlib, urllib
+
+from django.conf import settings
+from django.contrib.sites import models as sites_models
 
 import mongoengine
 from mongoengine.django import auth
 
-from piplmesh.account import fields
+from piplmesh.account import utils, fields
 
 LOWER_DATE_LIMIT = 366 * 120
 
@@ -17,6 +20,35 @@ class User(auth.User):
     
     facebook_id = mongoengine.IntField()
     facebook_token = mongoengine.StringField(max_length=150)
+    
+    def profile_image_url(self, size='square', request=None, is_secure=False, default_url=''):
+        """
+        Returns users profile image url.
+        
+        ``size`` can have a value of "square" (50x50), "small" (50 pixels wide, variable height), 
+        "normal" (100 pixels wide, variable height) and "large" (200 pixels wide, variable height).
+        
+        `default_url`` must be whole url (eg. www.wlan-si.net/images/default.png) without ``http://``
+        """
+        
+        if request:
+            if sites_models.Site._meta.installed:
+                domain = sites_models.Site.objects.get_current().domain
+            else:
+                domain = sites_models.RequestSite(request).domain
+                
+            is_secure = request.is_secure()
+            default_url = '%s/%s/piplmesh/images/unknown.png' % (domain, settings.STATIC_URL)
+        
+        if self.facebook_id:
+            return '%s?type=%s' % (utils.graph_api_url('%s/picture' % self), size)
+        else:
+            return 'http%(secure)s://secure.gravatar.com/avatar/%(email_hash)s?s=%(size)s&d=http%(secure)s%(default_url)s' % {
+                'secure': 's' if is_secure else '',
+                'email_hash': hashlib.md5(self.email.lower()).hexdigest(),
+                'size': (50, 50, 100, 200)[('square', 'small', 'normal', 'large').index(size)],
+                'default_url': urllib.quote('://' + default_url),
+            }
 
 class Comment(mongoengine.EmbeddedDocument):
     """
--- piplmesh/account/templatetags/user.py
+++ piplmesh/account/templatetags/user.py
@@ -1,21 +1,17 @@
 from django import template
 
-from piplmesh.account.templatetags import facebook
-from piplmesh.frontend.templatetags import gravatar
-
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
+@register.inclusion_tag('tags/profile_image.html', takes_context=True)
 def profile_image(context, user, size='square'):
     """
-    Returns img tag with users avatar.
+    Returns img tag with users profile image.
     
     Takes ``User`` object and optional size.
     Sizes include "square" (50x50), "small" (50 pixels wide, variable height), 
     "normal" (100 pixels wide, variable height) and "large" (200 pixels wide, variable height).
     """
     
-    if user.facebook_id:
-        return facebook.facebook_picture(user.username, size)
-    else:
-        return gravatar.gravatar(context, user, (50, 50, 100, 200)[('square', 'small', 'normal', 'large').index(size)])
+    return {
+        'profile_image_url': user.profile_image_url(size, context['request'])
+    }
